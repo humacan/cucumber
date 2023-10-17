b0c6817e88558b984333da096681cc0d36cd9f53
humacan <msgulluce@gmail.com>
2023-10-17 18:27:43 +0300

    cucumber ilk

---
+++ src/test/java/runners/Runner.java
@@ -0,0 +1,43 @@
+package runners;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+//{"html:target/cucumber-reports.html",
+//        "json:target/json-reports/cucumber.json",
+//        "junit:target/xml-report/cucumber.xml"},
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        plugin = "html:target/cucumber-reports.html",
+        features = "src/test/resources/features",
+        glue="stepdefinitions",
+        tags="@wip",
+        dryRun = false
+
+)
+public class Runner {
+      /*
+        Runner class'i bos bir class'dir
+
+        bu class'da asil isi
+
+        Class'da kullandigimiz 2 notasyon yapar
+
+        dryRun = true secildiginde
+        Runner istenen featur/scenario'yu calistirmaya degil
+        eksik adimlarini bulmaya odaklanir
+
+        eger eksik adim yoksa
+        test PASSED der.
+        ama bu testin calisip, tum adimlarin gectigini gostermez
+        SADECE eksik adim olmadigini gosterir
+
+        EGER eksik adim bulma amacimiz yoksa
+        testlerimizi normal olarak CALISTIRMAK istiyorsak
+        dryRun = false secilmelidir
+     */
+
+
+
+}
